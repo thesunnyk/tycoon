cb79868c7f48efac863830f790c6a8886392370b
Sunny Kalsi <thesunnyk@gmail.com>
2015-11-09 16:21:11 +1100

    Moved to flatmaps

--- src/gameloop.rs
+++ src/gameloop.rs
@@ -1,12 +1,16 @@
 extern crate sdl2;
 
 use self::sdl2::Sdl;
+use self::sdl2::EventPump;
+use self::sdl2::SdlResult;
+use self::sdl2::TimerSubsystem;
 use self::sdl2::VideoSubsystem;
 use self::sdl2::event::Event;
 use self::sdl2::render::Renderer;
 use self::sdl2::render::Texture;
 
 use utils::FatalAction;
+use utils::ResultMap;
 use rendererutils::RendererUtils;
 
 fn draw_bg(renderer: &mut Renderer, background: &Texture) {
@@ -32,37 +36,45 @@ pub struct GameLoop {
     height: u32,
 }
 
+struct RunLoopData<'a> {
+    renderer: Renderer<'a>,
+    timer: TimerSubsystem,
+    events: EventPump
+}
+
 impl GameLoop {
-    pub fn new(width: u32, height: u32) -> GameLoop {
-        let context = sdl2::init().or_die("create SDL context");
-        let video = context.video().or_die("create SDL video");
-        GameLoop {
-            context: context,
-            width: width,
-            height: height,
-            video: video
-        }
+    pub fn new(width: u32, height: u32) -> SdlResult<GameLoop> {
+        sdl2::init().res_map(|context| context.video().map(|video| GameLoop {
+                context: context,
+                width: width,
+                height: height,
+                video: video
+            }))
     }
 
-    fn build_renderer(&self) -> Renderer {
-        let builder = self.video.window("Cretacious Island", self.width, self.height).build()
-            .or_die("create Window");
-        builder.renderer().accelerated().present_vsync().build()
-                .or_die("create Renderer")
+    fn build_renderer(&self) -> SdlResult<Renderer> {
+        self.video.window("Cretacious Island", self.width, self.height).build()
+            .res_map(|builder| builder.renderer().accelerated().present_vsync().build())
     }
 
-    pub fn run(&self) {
-        let mut renderer = self.build_renderer();
-        let mut timer = self.context.timer().or_die("create Timer");
-        let mut events = self.context.event_pump().or_die("create Events");
-        let background = renderer.load_bmp("background.bmp").or_die("create texture");
-        let image = renderer.load_bmp("image.bmp").or_die("create texture");
+    fn build_runloop_data(&self) -> SdlResult<RunLoopData> {
+        self.build_renderer().res_map(|renderer| self.context.timer()
+            .res_map(|timer| self.context.event_pump().map(|events| RunLoopData {
+                renderer: renderer,
+                timer: timer,
+                events: events
+            })))
+    }
+
+    pub fn do_run(&self, mut rd: RunLoopData) {
+        let background = rd.renderer.load_bmp("background.bmp").or_die("create texture");
+        let image = rd.renderer.load_bmp("image.bmp").or_die("create texture");
 
         let mut quit = false;
         while !quit {
-            let start_ticks = timer.ticks();
+            let start_ticks = rd.timer.ticks();
             // Events
-            for ev in events.poll_iter() {
+            for ev in rd.events.poll_iter() {
                 match ev {
                     Event::Quit {timestamp: _} => {
                         quit = true;
@@ -72,15 +84,19 @@ impl GameLoop {
             }
             // Logic
             // Rendering
-            renderer.clear();
-            draw_bg(&mut renderer, &background);
-            draw_fg(&mut renderer, &image, self.width, self.height);
-            renderer.present();
+            rd.renderer.clear();
+            draw_bg(&mut rd.renderer, &background);
+            draw_fg(&mut rd.renderer, &image, self.width, self.height);
+            rd.renderer.present();
 
-            while timer.ticks() - start_ticks < 1000 / 60 {
-                timer.delay(1);
+            while rd.timer.ticks() - start_ticks < 1000 / 60 {
+                rd.timer.delay(1);
             }
         }
+    }
+
+    pub fn run(&self) -> SdlResult<()> {
+        self.build_runloop_data().map(|rd| self.do_run(rd))
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ mod utils;
 mod rendererutils;
 
 use gameloop::GameLoop;
+use utils::FatalAction;
 
 fn main() {
-    let mainloop = GameLoop::new(640, 480);
+    let mainloop = GameLoop::new(640, 480).or_die("create Game Loop");
 
-    mainloop.run();
+    mainloop.run().or_die("run Game Loop");
 }
